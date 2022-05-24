db40f5dec99422c84445196b055c38efc490b8b4
chandrakala <chadrakalasv1502@gmail.com>
2022-05-24 22:07:43 +0530

    added code to find Euclidean distance

---
+++ com/bl/day5/DistanceJava.java
@@ -0,0 +1,15 @@
+package com.bl.day5;
+
+import java.util.Scanner;
+
+public class DistanceJava {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter x value ");
+        int x = sc.nextInt();
+        System.out.println("Enter y value");
+        int y = sc.nextInt();
+        double distance = Math.sqrt((x * x) + (y * y));
+        System.out.println("the distance is" + " " + distance);
+    }
+}
