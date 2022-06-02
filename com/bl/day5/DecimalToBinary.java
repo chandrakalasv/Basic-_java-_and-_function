c7b366518253d57c1e085f77571f1eeffdf84349
chandrakala <chadrakalasv1502@gmail.com>
2022-06-02 20:04:26 +0530

    added code to perform padding for the binary number

--- com/bl/day5/DecimalToBinary.java
+++ com/bl/day5/DecimalToBinary.java
@@ -1,7 +1,7 @@
 package com.bl.day5;
 
 import java.util.Scanner;
-public class DecimalToBinary {
+public class deDecimalToBinary {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Decimal number: ");
